2071fa207f8c5802860ddedaafbc1dda4871fedd
David Santacoloma <andres.santacoloma@ceiba.com.co>
2018-05-04 14:49:23 -0500

    Implementaciones vacías para el patrón DAO

---
+++ src/co/edu/udea/wi/dao/impl/AnswerDaoImpl.java
@@ -0,0 +1,34 @@
+package co.edu.udea.wi.dao.impl;
+
+import java.util.List;
+
+import co.edu.udea.wi.dao.AnswerDao;
+import co.edu.udea.wi.dto.Answer;
+
+public class AnswerDaoImpl implements AnswerDao {
+
+	@Override
+	public void add(Answer answer) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Answer answer) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Answer answer) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Answer> getAnswers() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/co/edu/udea/wi/dao/impl/RequestDaoImpl.java
@@ -0,0 +1,34 @@
+package co.edu.udea.wi.dao.impl;
+
+import java.util.List;
+
+import co.edu.udea.wi.dao.RequestDao;
+import co.edu.udea.wi.dto.Request;
+
+public class RequestDaoImpl implements RequestDao {
+
+	@Override
+	public void add(Request request) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Request request) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Request request) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Request> getRequests() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
