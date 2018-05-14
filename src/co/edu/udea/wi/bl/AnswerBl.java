eddfd009ce99d1dbe216cbd5ac2d842d770417bb
David Santacoloma <andres.santacoloma@ceiba.com.co>
2018-05-14 01:10:53 -0500

    Agregar comentarios a las interfaces de lógica de negocio

--- src/co/edu/udea/wi/bl/AnswerBl.java
+++ src/co/edu/udea/wi/bl/AnswerBl.java
@@ -2,8 +2,24 @@ package co.edu.udea.wi.bl;
 
 import co.edu.udea.wi.dto.Answer;
 
+/**
+ * 
+ * @author david_santacoloma
+ * Description: Esta clase se utiliza para gestionar la creación y actualización de solicitudes de los usuarios
+ */
 public interface AnswerBl {
 
+	/**
+	 * Description: Este método se utiliza para crear solicitudes
+	 * @param answer
+	 * @throws Exception
+	 */
 	public void create(Answer answer) throws Exception;
+	
+	/**
+	 * Description: Este método se utiliza para actualizar solicitudes
+	 * @param answer
+	 * @throws Exception
+	 */
 	public void update(Answer answer) throws Exception;
 }
--- src/co/edu/udea/wi/bl/RequestBl.java
+++ src/co/edu/udea/wi/bl/RequestBl.java
@@ -2,8 +2,24 @@ package co.edu.udea.wi.bl;
 
 import co.edu.udea.wi.dto.Request;
 
+/**
+ * 
+ * @author david_santacoloma
+ * Description: Esta clase se utiliza para gestionar la creación y actualización respuestas a las solicitudes de los usuarios
+ */
 public interface RequestBl {
 
+	/**
+	 * Description: Este método se utiliza para crear respuestas
+	 * @param request
+	 * @throws Exception
+	 */
 	public void create(Request request) throws Exception;
+	
+	/**
+	 * Description: Este método se utiliza para actualizar respuestas
+	 * @param request
+	 * @throws Exception
+	 */
 	public void update(Request request) throws Exception;
 }
