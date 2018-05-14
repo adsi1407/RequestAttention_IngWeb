09aa482fbadb0ceb409e342e494ce149ba4a4e23
David Santacoloma <andres.santacoloma@ceiba.com.co>
2018-05-14 01:09:56 -0500

    Agregar comentarios a las interfaces de los Dao

--- src/co/edu/udea/wi/dao/AnswerDao.java
+++ src/co/edu/udea/wi/dao/AnswerDao.java
@@ -4,10 +4,38 @@ import java.util.List;
 
 import co.edu.udea.wi.dto.Answer;
 
+/**
+ * 
+ * @author david_santacoloma
+ * Description Esta clase se utiliza para acceder a las respuestas a las solicitudes de los usuarios
+ */
 public interface AnswerDao {
 
+	/**
+	 * Description Este método se utiliza para agregar una respuesta a una solicitud
+	 * @param answer
+	 * @throws Exception
+	 */
 	public void add(Answer answer) throws Exception;
+	
+	/**
+	 * Description Este método se utiliza para actualizar una respuesta a una solicitud
+	 * @param answer
+	 * @throws Exception
+	 */
 	public void update(Answer answer) throws Exception;
+	
+	/**
+	 * Description Este método se utiliza para eliminar una respuesta a una solicitud
+	 * @param answer
+	 * @throws Exception
+	 */
 	public void delete(Answer answer) throws Exception;
+	
+	/**
+	 * Description Este método se utiliza para obtener las respuestas a solicitudes
+	 * @return
+	 * @throws Exception
+	 */
 	public List<Answer> getAnswers() throws Exception;
 }
--- src/co/edu/udea/wi/dao/RequestDao.java
+++ src/co/edu/udea/wi/dao/RequestDao.java
@@ -4,10 +4,38 @@ import java.util.List;
 
 import co.edu.udea.wi.dto.Request;
 
+/**
+ * 
+ * @author david_santacoloma
+ * Description Esta clase se utiliza para acceder a las solicitudes de los usuarios
+ */
 public interface RequestDao {
 
+	/**
+	 * Description Este método se utiliza para agregar una solicitud
+	 * @param request
+	 * @throws Exception
+	 */
 	public void add(Request request) throws Exception;
+	
+	/**
+	 * Description Este método se utiliza para actualizar una solicitud
+	 * @param request
+	 * @throws Exception
+	 */
 	public void update(Request request) throws Exception;
+	
+	/**
+	 * Description Este método se utiliza para eliminar una solicitud
+	 * @param request
+	 * @throws Exception
+	 */
 	public void delete(Request request) throws Exception;
+	
+	/**
+	 * Description Este método se utiliza para obtener las solicitudes
+	 * @return
+	 * @throws Exception
+	 */
 	public List<Request> getRequests() throws Exception;
 }
