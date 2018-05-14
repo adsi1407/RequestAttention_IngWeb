e179aed3d5a514705cd154cc2917cfc10269b4f7
David Santacoloma <andres.santacoloma@ceiba.com.co>
2018-05-14 01:24:43 -0500

    Implementar Dao para las solicitudes y respuestas

--- src/co/edu/udea/wi/dao/impl/AnswerDaoImpl.java
+++ src/co/edu/udea/wi/dao/impl/AnswerDaoImpl.java
@@ -1,34 +1,95 @@
 package co.edu.udea.wi.dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import co.edu.udea.wi.dao.AnswerDao;
 import co.edu.udea.wi.dto.Answer;
 
-public class AnswerDaoImpl implements AnswerDao {
+public class AnswerDaoImpl extends HibernateDaoSupport implements AnswerDao {
 
 	@Override
 	public void add(Answer answer) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		Transaction transaction = null;
+		Session session = null;
+		
+		try {
+			
+			session = this.getSessionFactory().getCurrentSession();
+			transaction = session.beginTransaction();
+			session.save(answer);
+			transaction.commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public void update(Answer answer) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		Transaction transaction = null;
+		Session session = null;
+		
+		try {
+			
+			session = this.getSessionFactory().getCurrentSession();
+			transaction = session.beginTransaction();
+			session.update(answer);
+			transaction.commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public void delete(Answer answer) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		Transaction transaction = null;
+		Session session = null;
+		
+		try {
+			
+			session = this.getSessionFactory().getCurrentSession();
+			transaction = session.beginTransaction();
+			session.delete(answer);
+			transaction.commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public List<Answer> getAnswers() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Answer> answers = new ArrayList<Answer>();
+        Session session = null;
+        try{
+            session = this.getSessionFactory().getCurrentSession();
+            
+            Criteria criteria = session.createCriteria(Answer.class);
+            
+            answers = criteria.list();
+            
+        }catch(HibernateException e){
+            throw new Exception(e);
+        }finally{
+        	session.close();
+        }
+        
+        return answers;
 	}
 
 }
--- src/co/edu/udea/wi/dao/impl/RequestDaoImpl.java
+++ src/co/edu/udea/wi/dao/impl/RequestDaoImpl.java
@@ -1,34 +1,95 @@
 package co.edu.udea.wi.dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import co.edu.udea.wi.dao.RequestDao;
 import co.edu.udea.wi.dto.Request;
 
-public class RequestDaoImpl implements RequestDao {
+public class RequestDaoImpl extends HibernateDaoSupport implements RequestDao {
 
 	@Override
 	public void add(Request request) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		Transaction transaction = null;
+		Session session = null;
+		
+		try {
+			
+			session = this.getSessionFactory().getCurrentSession();
+			transaction = session.beginTransaction();
+			session.save(request);
+			transaction.commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public void update(Request request) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		Transaction transaction = null;
+		Session session = null;
+		
+		try {
+			
+			session = this.getSessionFactory().getCurrentSession();
+			transaction = session.beginTransaction();
+			session.update(request);
+			transaction.commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public void delete(Request request) throws Exception {
-		// TODO Auto-generated method stub
-
+		
+		Transaction transaction = null;
+		Session session = null;
+		
+		try {
+			
+			session = this.getSessionFactory().getCurrentSession();
+			transaction = session.beginTransaction();
+			session.delete(request);
+			transaction.commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public List<Request> getRequests() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Request> requests = new ArrayList<Request>();
+        Session session = null;
+        try{
+            session = this.getSessionFactory().getCurrentSession();
+            
+            Criteria criteria = session.createCriteria(Request.class);
+            
+            requests = criteria.list();
+            
+        }catch(HibernateException e){
+            throw new Exception(e);
+        }finally{
+        	session.close();
+        }
+        
+        return requests;
 	}
 
 }
