601e6d8cb2c8245fa3fe0a0a688001bbdcdc0b68
David Santacoloma <andres.santacoloma@ceiba.com.co>
2018-05-20 15:18:06 -0500

    Clase vacía para las validaciones de solicitudes

---
+++ src/co/edu/udea/wi/validations/RequestValidations.java
@@ -0,0 +1,13 @@
+package co.edu.udea.wi.validations;
+
+import co.edu.udea.wi.dto.Request;
+
+public class RequestValidations {
+
+	public boolean validateRequest(Request request) {
+		
+		boolean response = true;
+		
+		return response;
+	}
+}
