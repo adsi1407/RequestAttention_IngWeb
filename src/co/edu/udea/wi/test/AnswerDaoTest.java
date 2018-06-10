7bae1642818d0f6b8f70647f1b0707750330005c
David Santacoloma <adsi1409@hotmail.com>
2018-06-10 18:16:54 -0500

    Agregar pruebas unitarias de clientes y respuestas

--- src/co/edu/udea/wi/test/AnswerDaoTest.java
+++ src/co/edu/udea/wi/test/AnswerDaoTest.java
@@ -2,6 +2,72 @@ package co.edu.udea.wi.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.udea.wi.dao.AnswerDao;
+import co.edu.udea.wi.dto.Answer;
+import co.edu.udea.wi.dto.Attendant;
+import co.edu.udea.wi.dto.Customer;
+import co.edu.udea.wi.dto.Request;
+import co.edu.udea.wi.dto.RequestState;
+import co.edu.udea.wi.dto.RequestType;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:SpringConfig.xml")
+@Transactional
 public class AnswerDaoTest {
 
+	@Autowired
+	AnswerDao dao;
+	
+	@Test
+	public void addTest() {
+		
+		Attendant attendant = new Attendant();
+		attendant.setFirstName("Test Name");
+		attendant.setLastName("Test LastName");
+		attendant.setPhoneNumber("3001112233");
+		attendant.setEmail("prueba@prueba.com.co");
+		attendant.setPosition("Advisor");
+		
+		Customer customer = new Customer();
+		customer.setFirstName("Customer Name");
+		customer.setLastName("Customer LastName");
+		customer.setAddress("Calle 1");
+		customer.setEmail("customer@prueba.com.co");
+		customer.setPhoneNumber("3013335577");
+		
+		RequestState requestState = new RequestState();
+		requestState.setName("Open");
+		
+		RequestType requestType = new RequestType();
+		requestType.setName("Suggestion");
+		
+		Request request = new Request();
+		request.setApplicationDate(new Date());
+		request.setCustomer(customer);
+		request.setDescription("test description");
+		request.setState(requestState);
+		request.setType(requestType);
+		
+		Answer answer = new Answer();
+		answer.setAnswerDate(new Date());
+		answer.setDescription("test");
+		answer.setAttendant(attendant);
+		answer.setRequest(request);
+		
+		try {
+			dao.add(answer);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
 }
---
+++ src/co/edu/udea/wi/test/CustomerDaoTest.java
@@ -0,0 +1,61 @@
+package co.edu.udea.wi.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.udea.wi.dao.CustomerDao;
+import co.edu.udea.wi.dto.Customer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:SpringConfig.xml")
+@Transactional
+public class CustomerDaoTest {
+
+	@Autowired
+	CustomerDao dao;
+	
+	@Test
+	public void addTest() {
+		
+		Customer customer = new Customer();
+		customer.setID("1234567890");
+		customer.setFirstName("Customer Name");
+		customer.setLastName("Customer LastName");
+		customer.setAddress("Calle 1");
+		customer.setEmail("customer@prueba.com.co");
+		customer.setPhoneNumber("3013335577");
+		
+		try {
+			dao.add(customer);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void getCustomersTest() {
+		
+		List<Customer> customers = new ArrayList<Customer>();
+		
+		try {
+			
+			customers = dao.getCustomers();
+			assertTrue(customers.size() > 0);
+						
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+}
