65f1e8ee6b8114dc5879da1104b027a2d30d4e71
David Santacoloma <adsi1409@hotmail.com>
2018-06-10 21:51:23 -0500

    Agregar método en excepción personalizada para mostrar el mensaje con formato

--- src/co/edu/udea/wi/exception/ClassException.java
+++ src/co/edu/udea/wi/exception/ClassException.java
@@ -1,5 +1,7 @@
 package co.edu.udea.wi.exception;
 
+import java.util.List;
+
 public class ClassException extends Exception {
 
 	public ClassException() {
@@ -26,4 +28,11 @@ public class ClassException extends Exception {
 		// TODO Auto-generated constructor stub
 	}
 
+	public ClassException(List<String> errors) {
+		super(createFormattedMessage(errors));
+	}
+	
+	private static String createFormattedMessage(List<String> errors) {
+		return "Errores:" + System.getProperty("line.separator") + "- " + String.join(System.getProperty("line.separator") + "- ", errors);
+	}
 }
